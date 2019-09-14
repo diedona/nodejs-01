f2122a3c17cbf02d4d55a0177af92f4c45affeb6
Diego Doná <diedona@gmail.com>
2019-09-14 10:58:55 -0300

    crud in product controller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+mongoose.set('useFindAndModify', false);
+
 const product = mongoose.model("Product");
 
 module.exports = {
@@ -6,8 +8,20 @@ module.exports = {
         const prods = await product.find();
         return res.json(prods);
     },
+    async detail(req, res) {
+        const productDetail = await product.find({_id:req.params.id}); //mongoose.findById
+        return res.json(productDetail);
+    },
     async save(req, res) {
         const newProduct = await product.create(req.body);
         return res.json(newProduct);
+    },
+    async update(req, res) {
+        const updatedProduct = await product.findByIdAndUpdate(req.params.id, req.body, { new: true});
+        return res.json(updatedProduct);
+    },
+    async delete(req, res) {
+        await product.findByIdAndDelete(req.params.id);
+        res.send();
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const routes = express.Router();
 
 const productController = require('./controllers/ProductController');
 routes.get("/product", productController.index);
+routes.get("/product/:id", productController.detail);
 routes.post("/product", productController.save);
+routes.put("/product/:id", productController.update);
+routes.delete("/product/:id", productController.delete);
 
 //
 

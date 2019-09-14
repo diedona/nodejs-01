8fc737092254d054a4706ebc3dcae980873c4edf
Diego Doná <diedona@gmail.com>
2019-09-14 11:18:10 -0300

    cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors");
 
 // inicia aplicação
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //inicia DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true, useUnifiedTopology: true});

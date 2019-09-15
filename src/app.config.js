cd6321c04ba50688c34f33f59abddb758373578f
Diego Doná <diedona@gmail.com>
2019-09-15 02:00:59 -0300

    remake

--- src/app.config.js
+++ src/app.config.js
@@ -1,31 +1,13 @@
 require('dotenv').config(); //reads .env file
-const convict = require('convict'); //multiple environment
 
-const config = convict({
-    env: {
-        doc: "The application environment.",
-        format: ["prod", "dev"],
-        default: "development",
-        env: "NODE_ENV"
-    },
+const config = {
     app: {
-        port: {
-            doc: "app port.",
-            format: '*',
-            default: "3001",
-        }
+        port: "3001"
     },
     db: {
-        connectionString: {
-            doc: "db connection string.",
-            format: '*',
-            default: "mongodb://localhost:27017/nodeapi",
-        }
+        connectionString: process.env.CONNECTION_STRING
     }
-});
+};
 
-const env = config.get('env');
-config.loadFile(`./src/config/${env}.json`); // starts at root
-config.validate({ allowed: 'strict' }); // throws error if config does not conform to schema
 
-module.exports = config.getProperties(); // so we can operate with a plain old JavaScript object and abstract away convict (optional)
+module.exports = config;
